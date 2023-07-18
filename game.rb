9bf73ae40e1f142c183501bf8d33c6838bfbea14
james <thuranira611@gmail.com>
2023-07-18 11:19:38 +0300

    create madlibs game

---
+++ game.rb
@@ -0,0 +1,13 @@
+def start()
+puts "Enter first adjective:"
+firstOne = gets.chomp()
+puts "Enter second adjective:"
+secondOne = gets.chomp()
+puts "Enter third adjective:"
+thirdOne = gets.chomp()
+puts "Enter your name:"
+nameOne = gets.chomp()
+
+puts "Roses are " +firstOne +", Violets are " +secondOne +", peace " +thirdOne +" and i'm " +nameOne +" ,and i will follow thuranira611 on Github"
+end
+start()
